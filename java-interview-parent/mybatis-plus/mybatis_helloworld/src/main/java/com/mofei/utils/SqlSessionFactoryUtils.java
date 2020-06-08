45c234c90be9d996dd38898cf1a3f82cf4fe67c3
mofei <mof@seirobotics.net>
2020-06-08 17:20:45 +0800

    [add]:[interface]:1.增加sqlSessionFactoryUtils工具类

---
+++ java-interview-parent/mybatis-plus/mybatis_helloworld/src/main/java/com/mofei/utils/SqlSessionFactoryUtils.java
@@ -0,0 +1,46 @@
+package com.mofei.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author mofei
+ * @date 2020/6/8 16:56
+ */
+public class SqlSessionFactoryUtils {
+
+    //增加两个方法：获取SqlSessionFactory   , 同时openSession（） ，
+    // 要求 ，多线程环境下如何实现单例模式 ，
+    private static SqlSessionFactory sqlSessionFactory = null;
+    private final static Class CLASS_LOCK = SqlSessionFactoryUtils.class;
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        String resource = "mybatis-config.xml";
+        InputStream resourceAsStream = null;
+        try {
+            resourceAsStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            Logger.getLogger(SqlSessionFactoryUtils.class.getName()).log(Level.SEVERE,null,e);
+        }
+        synchronized (CLASS_LOCK) {
+            if (sqlSessionFactory == null) {
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    public static SqlSession openSqlSession() {
+        if (sqlSessionFactory == null) {
+            getSqlSessionFactory();
+        }
+        return sqlSessionFactory.openSession();
+    }
+}
