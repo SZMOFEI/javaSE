707c1ddf7a2660caa944a4aed98e163f9601af8f
mofei <mof@seirobotics.net>
2020-09-10 09:18:45 +0800

    [add]:[all]:1.实现JDBCTemplate 操作数据库

---
+++ java-interview-parent/aiguigu-spring-demo/spring7-jdbctemplate/src/main/java/com/mofei/User.java
@@ -0,0 +1,56 @@
+package com.mofei;
+
+/**
+ * @author mofei
+ * @date 2020/9/9 20:42
+ */
+public class User {
+    private int id ;
+    private int age ;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+}
